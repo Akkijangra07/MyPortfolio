1cd6fe7f13de1743e277e982546009d475666d18
Akkijangra07 <ankitjangra847@gmail.com>
2024-06-13 17:18:17 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
-import Intro from './Components/Intro/Intro'
-import Skills from './Components/Skills/Skills'
-import Works from './Components/Works/Works'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import MyProjects from './Components/myProjects/Resume'
+
 import { Outlet } from 'react-router-dom'
 
 
