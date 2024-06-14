b7c498d4f486e6d649e66d321c8a64d1b0ffdc8b
Akkijangra07 <ankitjangra847@gmail.com>
2024-06-14 16:09:12 +0530

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,14 +17,14 @@ function Navbar() {
             <NavLink  to="/MyPortfolio/works" className='desktopMenuListItem'>Portfolio</NavLink>
             <NavLink  to='/MyPortfolio/resume' className='desktopMenuListItem'>Resume</NavLink>
         </div>
-        <Link to='/contact'><button className="desktopMenuBtn"><i className="fa-regular fa-message" style={{marginRight:"0.5rem"}}></i>Contact me</button></Link>
+        <Link to='/MyPortfolio/contact'><button className="desktopMenuBtn"><i className="fa-regular fa-message" style={{marginRight:"0.5rem"}}></i>Contact me</button></Link>
         <button className='mobMenu' onClick={()=>setShowMenu(!showMenu)}><i className="fa-solid fa-bars"></i></button>
         <div className="navMenu" style={{display: showMenu?"flex":"none"}}>
             <NavLink activeClass='active' to="/"  className='listItem' onClick={()=>setShowMenu(false)}>Home</NavLink>
-            <NavLink activeClass='active' to="/about" className='listItem' onClick={()=>setShowMenu(false)}>About</NavLink>
-            <NavLink activeClass='active' to="/resume" className='listItem' onClick={()=>setShowMenu(false)}>Resume</NavLink>
-            <NavLink activeClass='active' to="/works" className='listItem' onClick={()=>setShowMenu(false)}>Portfolio</NavLink>
-            <NavLink activeClass='active' to="/contact" className='listItem' onClick={()=>setShowMenu(false)}>Contact</NavLink>
+            <NavLink activeClass='active' to="/MyPortfolio/about" className='listItem' onClick={()=>setShowMenu(false)}>About</NavLink>
+            <NavLink activeClass='active' to="/MyPortfolio/resume" className='listItem' onClick={()=>setShowMenu(false)}>Resume</NavLink>
+            <NavLink activeClass='active' to="/MyPortfolio/works" className='listItem' onClick={()=>setShowMenu(false)}>Portfolio</NavLink>
+            <NavLink activeClass='active' to="/MyPortfolio/contact" className='listItem' onClick={()=>setShowMenu(false)}>Contact</NavLink>
 
         </div>
     </nav>
